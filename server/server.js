47a4221d7f1e259bac309ace01bd5bb33d86086d
Tinwana <125630233+Tinwana@users.noreply.github.com>
2023-09-07 21:31:59 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -14,10 +14,6 @@ const mongoDb = process.env.MONGO_DB;
 //app start
 const app = express();
 
-// Đường dẫn đến ứng dụng React
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
 
 //middleware
 app.use(express.json(), express.urlencoded({ extended : false }));
