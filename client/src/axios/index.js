c87adcc2f456176e75a59fee17c6f9fdd7fc0e41
Tinwana <125630233+Tinwana@users.noreply.github.com>
2023-10-08 15:04:18 +0700

    change api

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 //http://localhost:3001
 //https://mia-manager.onrender.com
-
+//https://mia-manager-backend.onrender.com
 export const axiosApi = axios.create({
-    baseURL:'https://mia-manager.onrender.com/api/v1/'
+    baseURL:'https://mia-manager-backend.onrender.com/api/v1/'
 })
